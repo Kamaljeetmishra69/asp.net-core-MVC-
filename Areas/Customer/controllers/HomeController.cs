06c9c538d1cc8295241d256362a767e1ae99b766
kamaljeetMishra69 <kamaljeetmishra69@gmail.com>
2026-07-17 16:58:53 +0530

    home ui with details page completed

--- Areas/Customer/controllers/HomeController.cs
+++ Areas/Customer/controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using udemy.Business.Services;
+using udemy.Business.Services.IServices;
 using Udemy.Models;
 
 namespace Area.Customer.controllers
@@ -7,27 +9,22 @@ namespace Area.Customer.controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IProductServices _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IProductServices productservice)
         {
-            _logger = logger;
+            _productService = productservice;
+        }
+        public  async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllProductAsync(includeCategory:true);
+            return View(products);
+        }
+        public  async Task<IActionResult> ProductDetail(int productid)
+        {
+            var product =  await _productService.GetProductByIdAsync(productid ,includeCategory:true );
+            return View(product);
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
